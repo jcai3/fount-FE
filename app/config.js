f9c3de3ca1f3876d6b3f146793f826e504ccdde6
wzhang0 <wzhang0@searshc.com>
2015-11-20 04:22:48 -0600

    add api endpoint for staging env

--- app/config.js
+++ app/config.js
@@ -6,6 +6,7 @@ angular.module('config', [])
   sharingHost: 'http://localhost:8100/#/media/',
   apiEnvname: 'local',
   apiEndpoint: 'http://localhost:9090/',
+  // apiEndpoint: 'https://spreest.searshc.com/lifestyle/',
   instagramRedirectDomain: 'http://localhost:9090/',
   instagramRedirectUri: 'getInstagramAccessToken',
   instagramClientId: '7d5af766cffa46c3b045dd5133001533',
