f27747e86972f45bf8740d6206f1ad2d817bb0b9
wzhang0 <wzhang0@searshc.com>
2016-01-15 14:47:03 -0600

    changes for admin sellers

--- www/js/controllers/admin-seller.js
+++ www/js/controllers/admin-seller.js
@@ -1,11 +1,19 @@
 'use strict';
 
 angular.module('sywStyleXApp')
-.controller('AdminSellerCtrl', ['$scope', '$state', 'localStorageService', 'UtilityService', 'AdminService', function($scope, $state, localStorageService, UtilityService, AdminService) {
+.controller('AdminSellerCtrl', ['$scope', '$state', 'UtilityService', 'AdminService', function($scope, $state, UtilityService, AdminService) {
   var getAdminSellers = function() {
     AdminService.getAdminSellers().then(function(res) {
       if (UtilityService.validateResult(res)) {
         $scope.adminSellers = res.data.payload.SELLERS;
+      }
+    });
+  };
+
+  $scope.updateAdminSeller = function() {
+    AdminService.updateAdminSellers($scope.adminSellers).then(function(res) {
+      if (UtilityService.validateResult(res)) {
+        $state.go($state.current, {}, {reload: true});
       }
     });
   };
